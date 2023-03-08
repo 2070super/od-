0de0130b98cbbe3d1910bd8f1ed335fb76da2ece
2070super <1025404072@qq.com>
2023-03-08 19:36:57 +0800

    Lily上课时使用字母数字图片教小朋友们学习英语单词，每次都需要把这些图片按照大小（ASCII码值从小到大）排列收好。请大家给Lily帮忙，通过代码解决。 Lily使用的图片使用字符"A"到"Z"、"a"到"z"、"0"到"9"表示。
    数据范围：每组输入的字符串长度满足
    1
    ≤
    �
    ≤
    1000
    
    1≤n≤1000

---
+++ 华为机试/HJ34.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin, s);
+    auto it = s.begin();
+    while (it != s.end())
+    {
+        if (*it == ' ')
+        {
+            it = s.erase(it);
+        }
+        it++;
+    }
+    sort(s.begin(), s.end());
+    cout << s;
+}
+// 64 位输出请用 printf("%lld")
